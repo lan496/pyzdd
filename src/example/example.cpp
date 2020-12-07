7ec8ddcdd7433e85629b5edebcf76d11a38499db
lan496 <kohei19950508@gmail.com>
2020-12-07 08:56:42 +0000

    create example 3x2 supercell

--- src/example/example.cpp
+++ src/example/example.cpp
@@ -21,52 +21,67 @@ using namespace pyzdd::derivative_structure;
 
 int main() {
     tdzdd::MessageHandler::showMessages(false);
+
+    // system
     int num_sites = 6;
     int num_types = 2;
     auto t1 = Permutation(std::vector<Element>{2, 0, 1, 5, 3, 4});
     auto t2 = Permutation(std::vector<Element>{3, 4, 5, 0, 1, 2});
     auto m = Permutation(std::vector<Element>{0, 2, 1, 3, 5, 4});
     auto automorphism = generate_group(std::vector<Permutation>{t1, t2, m});
-    auto translations = generate_group(std::vector<Permutation>{t1, t2});
     assert(automorphism.size() == 12);
 
-    // composition constraints
-    tdzdd::DdStructure<2> dd;
-    /*
-    pyzdd::derivative_structure::construct_derivative_structures(
-        dd,
-        num_sites,
-        num_types,
-        automorphism,
-        translations,
-        std::vector<int>(),
-        std::vector<std::vector<permutation::Element>>(),
-        false,
-        false
-    );
-    dd.dumpDot(std::cerr);
-    */
-
-    std::vector<std::pair<std::vector<int>, int>> composition_constraints = {
-        std::make_pair(std::vector<int>{0, 1, 2, 3, 4, 5}, 3)
-    };
-
+    // cluster graph
     int num_variables = num_sites;
     Graph cluster_graph(num_variables);
     add_undirected_edge(cluster_graph, 0, 1, 1);
-    add_undirected_edge(cluster_graph, 0, 3, 1);
+    add_undirected_edge(cluster_graph, 0, 3, 2);
     add_undirected_edge(cluster_graph, 1, 2, 1);
-    add_undirected_edge(cluster_graph, 1, 5, 1);
+    add_undirected_edge(cluster_graph, 1, 4, 2);
     add_undirected_edge(cluster_graph, 2, 0, 1);
-    add_undirected_edge(cluster_graph, 2, 3, 1);
-    // auto vertex_order = get_vertex_order_by_bfs(cluster_graph);
-    std::vector<pyzdd::graph::Vertex> vertex_order = {0, 1, 2, 3, 4, 5};
+    add_undirected_edge(cluster_graph, 2, 5, 2);
+    add_undirected_edge(cluster_graph, 3, 4, 1);
+    add_undirected_edge(cluster_graph, 3, 5, 1);
+    add_undirected_edge(cluster_graph, 4, 5, 1);
+    auto vertex_order = get_vertex_order_by_bfs(cluster_graph);
     for (auto v: vertex_order) {
         std::cerr << " " << v;
     }
     std::cerr << std::endl;
-
     int target = 3;
+
+    // full structures
+    {
+        BinaryVertexConverter converter(num_sites, vertex_order);
+        std::vector<Permutation> automorphism_aug;
+        for (const auto& perm: automorphism) {
+            auto aug = converter.reorder_premutation(perm);
+            automorphism_aug.emplace_back(aug);
+        }
+
+        tdzdd::DdStructure<2> dd0;
+        std::vector<Permutation> translations_aug;
+        pyzdd::derivative_structure::construct_derivative_structures(
+            dd0,
+            num_sites,
+            num_types,
+            automorphism_aug,
+            std::vector<Permutation>(),
+            std::vector<int>(),
+            std::vector<std::vector<permutation::Element>>(),
+            false,  // incomplete
+            false  // superperiodic
+        );
+
+        std::ofstream os("full.dot");
+        dd0.dumpDot(os);
+    }
+
+    // composition constraints
+    tdzdd::DdStructure<2> dd;
+    std::vector<std::pair<std::vector<int>, int>> composition_constraints = {
+        std::make_pair(std::vector<int>{0, 1, 2, 3, 4, 5}, 3)
+    };
 
     pyzdd::derivative_structure::prepare_derivative_structures_with_sro(
         dd,
@@ -76,11 +91,9 @@ int main() {
         automorphism,
         composition_constraints
     );
-
-    std::ofstream os1("example.structure.dot");
+    std::ofstream os1("AB.dot");
     dd.dumpDot(os1);
 
-    /*
     pyzdd::derivative_structure::restrict_pair_correlation(
         dd,
         num_sites,
@@ -89,7 +102,8 @@ int main() {
         cluster_graph,
         target
     );
-    */
+    std::ofstream os2("AB_1stNN.dot");
+    dd.dumpDot(os2);
 
     return 0;
 }
